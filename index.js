54ca262ef63f76fb6de3a5875869b197be0f068f
Niloy-Dey <niloydeyce@gmail.com>
2022-05-08 10:04:36 +0600

    get data and successfully running database

--- index.js
+++ index.js
@@ -26,7 +26,12 @@ async function run(){
 
 
     // getting data form database
-
+    app.get('/products' , async(req, res) =>{
+        const query = {};
+        const cursor = productCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products)
+    })
 
     }
 
