f6c7c47c75fb7834f4ac58f4f1048a6e1373cd67
Niloy-Dey <niloydeyce@gmail.com>
2022-05-08 10:03:31 +0600

    database is create done

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+//middle ware 
+app.use(cors());
+app.use(express.json());
+
+// waltonRefregerator
+// g61r3D9mThKnItVa
+
+
+
+const uri = "mongodb+srv://waltonRefregerator:g61r3D9mThKnItVa@cluster0.clyrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+    const productCollection = client.db('waltonRefregerator').collection('products');
+
+
+
+    // getting data form database
+
+
+    }
+
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('walton refrigerator database is running');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port 5000`);
+})
